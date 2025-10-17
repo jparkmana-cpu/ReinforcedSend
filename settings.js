afe726cd137d6dae944542098a58f19e3befe0af
jparkmana-cpu <jpark.mana@gmail.com>
2025-10-18 01:31:19 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-import { getContext, extension_settings, extension_path } from "../../../extensions.js";
+import { getContext, extension_settings } from "../../../extensions.js";
 
 const extension_name = "ReinforcedSend";
 
